9944b4d6075a48f60941bebdfaeddc553e7e148f
yiqil <yiqil5@163.com>
2021-05-23 17:30:04 +0800

    first commit

--- src/main/lyq.go
+++ src/main/lyq.go
@@ -4,8 +4,28 @@ package main
 import "fmt"
 
 
+// for sorting by key.
+type ByKey []KV
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
+
+
 func main(){
 
 	values:= []string{"1","2"}
 	fmt.Println(len(values))
+	kv = KV{'shiki',2}
+	fmt.Println(kv)
+
+
+
 }
+
+
+type KV struct {
+	k string
+        v int
+      }
